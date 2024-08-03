e6a490d9c3dd32c1ad5e0e4f766385f137d1eb23
jinpold <jinpold@gmail.com>
2024-08-04 01:10:58 +0900

    240804

--- service/admin-service/src/main/java/store/ggun/admin/repository/jpa/AdminRepository.java
+++ service/admin-service/src/main/java/store/ggun/admin/repository/jpa/AdminRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface AdminRepository extends JpaRepository<AdminModel, Long> {
     Optional<AdminModel> findAdminByUsername(String enpName);
-    @Query("SELECT a FROM Admins a WHERE a.email = :email")
+    @Query("SELECT a FROM admins a WHERE a.email = :email")
     Optional<AdminModel> findAdminByEmail(@Param("email") String email);
     Optional<AdminModel> findAdminByRole(String Role);
 
