b9eafeaf0c6a68124aad5e9f512411c766ace95f
jinpold <jinpold@gmail.com>
2024-08-04 11:01:55 +0900

    240804

--- service/admin-service/src/main/java/store/ggun/admin/filter/AuthenticationFilter.java
+++ service/admin-service/src/main/java/store/ggun/admin/filter/AuthenticationFilter.java
@@ -1,39 +1,39 @@
-package store.ggun.admin.filter;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.io.IOException;
-
-@Slf4j
-public class AuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
-    private Environment environment;
-    public AuthenticationFilter(AuthenticationManager authenticationManager,
-                                Environment environment) {
-        super(authenticationManager);
-        this.environment = environment;
-    }
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
-            throws AuthenticationException {
-        try {
-            return null;
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Override
-    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
-                                            Authentication auth) throws IOException, ServletException {
-        super.successfulAuthentication(req, res, chain, auth);
-    }
-}
+//package store.ggun.admin.filter;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.core.env.Environment;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import java.io.IOException;
+//
+//@Slf4j
+//public class AuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
+//    private Environment environment;
+//    public AuthenticationFilter(AuthenticationManager authenticationManager,
+//                                Environment environment) {
+//        super(authenticationManager);
+//        this.environment = environment;
+//    }
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+//            throws AuthenticationException {
+//        try {
+//            return null;
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+//                                            Authentication auth) throws IOException, ServletException {
+//        super.successfulAuthentication(req, res, chain, auth);
+//    }
+//}
